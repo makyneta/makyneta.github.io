7541e921e703bf5938b09d98fc51ae044efaebbb
Tomás Mota <208397927+tomasmotahq@users.noreply.github.com>
2026-03-15 00:36:32 +0000

    March 15, 2026

--- assets/scripts/dev.js
+++ assets/scripts/dev.js
@@ -2,6 +2,7 @@ const GH = `<svg class="ico-gh" viewBox="0 0 24 24"><path d="M12 0C5.37 0 0 5.37
 const EXT = `<svg class="ico-ext" viewBox="0 0 24 24"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>`;
 
 const projects = [
+  { title:"Nicholas Simões",       type:"frontend",  img:"/assets/images/projects/dev/nicholassimoes.jpg",                  code:"https://github.com/makyneta/nicolassimoes",                                                 demo:"https://makyneta.github.io/nico" },
   { title:"Amalias",               type:"frontend",  img:"/assets/images/projects/dev/amalias.jpg",                         code:"https://github.com/makyneta/amalias",                                                       demo:"https://makyneta.github.io/amalias" },
   { title:"Mayday",                type:"frontend",  img:"/assets/images/projects/dev/mayday.jpg",                          code:"https://github.com/makyneta/mayday",                                                        demo:"https://makyneta.github.io/mayday" },
   { title:"Nickz",                 type:"frontend",  img:"/assets/images/projects/dev/nickz.jpg",                           code:"https://github.com/makyneta/nickz",                                                         demo:"https://makyneta.github.io/nickz" },
