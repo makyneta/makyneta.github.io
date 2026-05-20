760c10c13bb102316248380e326b81ee66ad08f3
tomasmotadev <tomasmotadev@proton.me>
2026-05-20 23:58:32 +0100

    May 20, 2026

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -41,6 +41,25 @@ fetch("/assets/header/portuguese.html")
     document.body.appendChild(script);
   });
 
+//
+// Header-ES
+fetch("/assets/header/spanish.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("headeres").innerHTML = data;
+
+    // CSS
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "/assets/styles/header.css";
+    document.head.appendChild(link);
+
+    // JS
+    const script = document.createElement("script");
+    script.src = "/assets/scripts/header.js";
+    document.body.appendChild(script);
+  });
+
 
 //
 // Footer
